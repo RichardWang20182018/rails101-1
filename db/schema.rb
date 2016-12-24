4564ebaa4b6cd73949c0001d402895a322f70aa9
wangjian <sowhy.go@gmail.com>
2016-12-24 21:21:08 +0800

    create groups index

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 20161224082913) do
   create_table "groups", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
